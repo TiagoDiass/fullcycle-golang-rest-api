b1d2f0603441fdfc2a7c4a27b54841fdfe2bfe84
TiagoDiass <tiago.costadiasss@gmail.com>
2024-05-24 23:13:08 -0300

    test: finish tests of UserDB

--- internal/infra/database/user_db.go
+++ internal/infra/database/user_db.go
@@ -22,7 +22,7 @@ func (u *UserDB) Create(user *entity.User) error {
 func (u *UserDB) FindByEmail(email string) (*entity.User, error) {
 	var user entity.User
 
-	if err := u.DB.Where("email = ?").First(&user).Error; err != nil {
+	if err := u.DB.Where("email = ?", email).First(&user).Error; err != nil {
 		return nil, err
 	}
 
--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -9,7 +9,7 @@ import (
 	"gorm.io/gorm"
 )
 
-func TestCreateUser(t *testing.T) {
+func getDatabaseConnection(t *testing.T) *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
 
 	if err != nil {
@@ -18,10 +18,16 @@ func TestCreateUser(t *testing.T) {
 
 	db.AutoMigrate(&entity.User{})
 
+	return db
+}
+
+func TestCreateUser(t *testing.T) {
+	db := getDatabaseConnection(t)
+
 	user, _ := entity.NewUser("Tiago", "tiago@email.com", "123-password")
 	userDB := NewUserDB(db)
 
-	err = userDB.Create(user)
+	err := userDB.Create(user)
 	require.Nil(t, err)
 
 	var createdUser entity.User
@@ -32,4 +38,23 @@ func TestCreateUser(t *testing.T) {
 	require.Equal(t, createdUser.Name, user.Name)
 	require.Equal(t, createdUser.Email, user.Email)
 	require.NotEmpty(t, createdUser.Password)
+}
+
+func TestFindByEmail(t *testing.T) {
+	db := getDatabaseConnection(t)
+
+	createdUser, _ := entity.NewUser("Tiago", "tiago@email.com", "123-password")
+	userDB := NewUserDB(db)
+
+	err := userDB.Create(createdUser)
+	require.Nil(t, err)
+
+	foundUser, err := userDB.FindByEmail("tiago@email.com")
+
+	require.Nil(t, err)
+	require.NotNil(t, foundUser)
+	require.Equal(t, foundUser.ID, createdUser.ID)
+	require.Equal(t, foundUser.Name, createdUser.Name)
+	require.Equal(t, foundUser.Email, createdUser.Email)
+	require.Equal(t, foundUser.Password, createdUser.Password)
 }
