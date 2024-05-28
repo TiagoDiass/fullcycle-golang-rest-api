40e6fffa8faf439367995a77812a6dd9b8b7fade
TiagoDiass <tiago.costadiasss@gmail.com>
2024-05-27 21:22:15 -0300

    feat: create product handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,67 @@
 package main
 
-import "github.com/TiagoDiass/fullcycle-golang-rest-api/configs"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/TiagoDiass/fullcycle-golang-rest-api/configs"
+	"github.com/TiagoDiass/fullcycle-golang-rest-api/internal/dto"
+	"github.com/TiagoDiass/fullcycle-golang-rest-api/internal/entity"
+	"github.com/TiagoDiass/fullcycle-golang-rest-api/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	cfg, _ := configs.LoadConfig(".")
+	_, err := configs.LoadConfig(".")
 
-	println(cfg.DBName)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	http.ListenAndServe(":8000", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.IProductDB
+}
+
+func NewProductHandler(productDB database.IProductDB) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: productDB,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, req *http.Request) {
+	var createProductDTO dto.CreateProductInput
+	err := json.NewDecoder(req.Body).Decode(&createProductDTO)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product, err := entity.NewProduct(createProductDTO.Name, createProductDTO.Price)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Create(product)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
